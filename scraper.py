b3a98ae8efbfd19f2c61ab1de9f87812915898b4
Yijia Yan <yiy287@ucsd.edu>
2018-12-20 02:14:39 -0800

    add main function

--- scraper.py
+++ scraper.py
@@ -6,123 +6,131 @@ import requests as rq
 from bs4 import BeautifulSoup as bs
 import logging
 
-# log
-logging.basicConfig(
-  filename=os.path.abspath('.')+'/log',
-  format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-  datefmt='%H:%M:%S',
-  level=logging.DEBUG)
+def main():
+    # set up log
+    logging.basicConfig(
+        filename=os.path.abspath('.')+'/log',
+        format='%(asctime)s %(levelname)s %(message)s',
+        level=logging.DEBUG)
 
-def lg(msg=""):
-  logging.debug(msg)
-  print(msg)
-  
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-  
-lg("Scraper started")
-
-# create destination folder for download
-pics_dir = os.path.abspath('.') + '/pics/'
-if not os.path.exists(pics_dir):
-  os.makedirs(pics_dir)
-
-uids = input("Input targer usernames separated by space:\n").split()
-max_count = int(input("Input max number of downloads for each user:\n"))
-print()
-
-lg(("Target usernames = {}\n"
-    "Max downloads = {}").format(
-uids, max_count))
-lg()
-
-# iterate through each target user
-ucount = 0
-for u in uids:
-  lg("Now downloading from user: {}".format(u))
-  ucount += 1
-  
-  # create download folder
-  user_dir = pics_dir + u + '/'
-  if not os.path.exists(user_dir):
-    os.makedirs(user_dir)
-  
-  # iterate through gallery pages
-  count = 0
-  pg = 0
-  while True:
-    gallery_url = 'https://twicsy.com/u/' + u + '/skip/' + str(pg)
-    gallery_soup = bs(rq.get(gallery_url).content)
+    def lg(msg=""):
+        logging.debug(msg)
+        print(msg)
     
-    item_container = gallery_soup \
-    .find_all(class_='container inner_page')[0]
+    def clear():
+        os.system('cls' if os.name == 'nt' else 'clear')
     
-    if "No pics found!" in str(item_container):
-      lg("Reached end of gallery of user: " + u)
-      lg()
-      break
+    lg("Scraper started")
 
-    item_urls = item_container \
-    .find_all(class_='image_link trackerClass popper')
+    # create destination folder for download
+    pics_dir = os.path.abspath('.') + '/pics/'
+    if not os.path.exists(pics_dir):
+        os.makedirs(pics_dir)
 
-    item_urls = ['https://twicsy.com'+str(i).split('href="')[1].split('"')[0] \
-                 for i in item_urls]
+    uids = input("Input targer usernames separated by space:\n").split()
+    max_count = int(input("Input max number of downloads for each user:\n"))
+    print()
 
-    # visit item page
-    for item in item_urls:
-      count += 1
-      
-      # max count threshold check
-      if count > max_count:
-        lg("Reached max count!")
-        lg()
-        break
-      
-      clear()
-      lg("Progress: ({}/{}) @{} #{} (max {}) ".format(ucount, len(uids), u, count, max_count))
-      
-      item_meta = bs(ur.urlopen(item).read()) \
-      .find_all(id='outbound.main_pic')
+    lg(("Target usernames = {}\n"
+        "Max downloads = {}").format(
+    uids, max_count))
+    lg()
 
-      if len(item_meta) < 1:
-        lg("[X] Not a picture! ({})".format(item))
-        continue
-
-      item_meta = str(item_meta[0])
-
-      if 'pic.twitter.com' in item_meta:
-        image_url = item_meta.split('src="')[1].split('"')[0]
-        file_name = image_url.split('/')[-1].split(':')[0]
-
-      elif 'twitpic.com' in item_meta:
-        image_url = item_meta.split('href="')[1].split('"')[0]
-        image_meta = str(bs(rq.get(image_url).content))
-        image_url = image_meta.split('twitter:image" value="')
+    # iterate through each target user
+    ucount = 0
+    for u in uids:
+        lg("Now downloading from user: {}".format(u))
+        ucount += 1
         
-        if not len(image_meta):
-          lg("[X] Dead source link! ({})".format(item))
-          continue
+        # create download folder
+        user_dir = pics_dir + u + '/'
+        if not os.path.exists(user_dir):
+            os.makedirs(user_dir)
+        
+        # iterate through gallery pages
+        count = 0
+        pg = 0
+        while True:
+            gallery_url = 'https://twicsy.com/u/' + u + '/skip/' + str(pg)
+            gallery_soup = bs(rq.get(gallery_url).content)
+            
+            item_container = gallery_soup \
+            .find_all(class_='container inner_page')[0]
+            
+            if "No pics found!" in str(item_container):
+                lg("Reached end of gallery of user: " + u)
+                lg()
+                break
 
-        image_url = image_url[1].split('"')[0]
-        image_url = image_url.replace('&amp;', '&')
-        file_name = image_url.split('/')[-1].split('?')[0]
+            item_urls = item_container \
+            .find_all(class_='image_link trackerClass popper')
 
-      else:
-        lg("[X] Unknown source site! ({})".format(item))
-        continue
+            item_urls = ['https://twicsy.com'+str(i).split('href="')[1].split('"')[0] \
+                        for i in item_urls]
 
-      try:
-        ur.urlretrieve(image_url, user_dir + file_name)
-        lg("[O] Successful!")
+            # visit item page
+            for item in item_urls:
+                count += 1
+                
+                # max count threshold check
+                if count > max_count:
+                    lg("Reached max count!")
+                    lg()
+                    break
+                
+                clear()
+                lg("Progress: ({}/{}) @{} #{} (max {}) ".format(ucount, len(uids), u, count, max_count))
+                
+                item_meta = bs(ur.urlopen(item).read()) \
+                .find_all(id='outbound.main_pic')
 
-      except Exception as e:
-        lg("[X] Error: {} ({})".format(e, item))
-        continue
-    
-    if count > max_count:
-      break
-    
-    pg += 80
+                if len(item_meta) < 1:
+                    lg("[X] Not a picture! ({})".format(item))
+                    continue
 
-clear()
-lg("Completed")
+                item_meta = str(item_meta[0])
+
+                if 'pic.twitter.com' in item_meta:
+                    image_url = item_meta.split('src="')[1].split('"')[0]
+                    file_name = image_url.split('/')[-1].split(':')[0]
+
+                elif 'twitpic.com' in item_meta:
+                    image_url = item_meta.split('href="')[1].split('"')[0]
+                    image_meta = str(bs(rq.get(image_url).content))
+                    image_url = image_meta.split('twitter:image" value="')
+                    
+                    if not len(image_meta):
+                        lg("[X] Dead source link! ({})".format(item))
+                        continue
+
+                    image_url = image_url[1].split('"')[0]
+                    image_url = image_url.replace('&amp;', '&')
+                    file_name = image_url.split('/')[-1].split('?')[0]
+
+                else:
+                    lg("[X] Unknown source site! ({})".format(item))
+                    continue
+
+                try:
+                    ur.urlretrieve(image_url, user_dir + file_name)
+                    lg("[O] Successful!")
+
+                except Exception as e:
+                    lg("[X] Error: {} ({})".format(e, item))
+                    continue
+            # end of item iteration loop
+
+            if count > max_count:
+                break
+            
+            pg += 80
+        # end of gallery iteration loop
+
+    # end of user iteration loop
+
+    clear()
+    lg("Completed")
+# end of main()
+
+if __name__ == "__main__":
+    main()
